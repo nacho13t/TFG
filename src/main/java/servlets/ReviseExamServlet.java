b6794080fe968fef48177a3f54273e7321e37128
Nacho <nacho1714@gmail.com>
2018-02-24 14:09:34 +0000

    Tercer dia

--- src/main/java/servlets/ReviseExamServlet.java
+++ src/main/java/servlets/ReviseExamServlet.java
@@ -8,6 +8,9 @@ package servlets;
 import com.mycompany.multiplayerbiblio.User;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -30,7 +33,7 @@ public class ReviseExamServlet extends HttpServlet {
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+            throws ServletException, IOException, SQLException {
         response.setContentType("text/html;charset=UTF-8");
         int answersCount = Integer.parseInt(request.getParameter("answersCount"));
         int mark = 0;
@@ -62,7 +65,11 @@ public class ReviseExamServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        try {
+            processRequest(request, response);
+        } catch (SQLException ex) {
+            Logger.getLogger(ReviseExamServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     /**
@@ -76,7 +83,11 @@ public class ReviseExamServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        try {
+            processRequest(request, response);
+        } catch (SQLException ex) {
+            Logger.getLogger(ReviseExamServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     /**
